6278ae7c4fc0314e6c39fc6879a0a62911b1f970
Gustavo Teixeira <gustavo.pinho1@aluno.ce.gov.br>
2022-03-09 07:41:07 -0300

    Navbar + Product Card + Catalog + react-router-dom install

---
+++ src/pages/Catalog/index.tsx
@@ -0,0 +1,32 @@
+import Navbar from "components/Navbar";
+import ProductCard from "components/ProductCard";
+
+const Catalog = () => {
+    return (
+        <>
+        <Navbar />
+        <div className="container my-4">
+            <div className="row">
+<div className="col-sm-6 col-lg-4 col-x1-3">
+<ProductCard />
+</div>
+<div className="col-sm-6 col-lg-4 col-x1-3">
+<ProductCard />
+</div>
+<div className="col-sm-6 col-lg-4 col-x1-3">
+<ProductCard />
+</div>
+<div className="col-sm-6 col-lg-4 col-x1-3">
+<ProductCard />
+</div>
+<div className="col-sm-6 col-lg-4 col-x1-3">
+<ProductCard />
+</div>
+</div>
+
+
+</div>
+        </>
+    );
+};
+export default Catalog;
